0f8b6a360564ef0096fc2549d5c6cb9d9b7e980d
Sahar11 <saharhalim151@gmail.com>
2022-06-25 07:40:57 -0400

    add and style inbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Inbox from './components/inbox/Inbox.jsx';
+import Tabs from './components/tabs/Tabs.jsx';
 import Header from './Header.jsx';
 
 const App = () => {
   return (
     <div className='container'>
       <Header/>
+      <Tabs />
       <div className="container-view"><Inbox /></div>
     </div>
   );
--- src/components/inbox/Inbox.jsx
+++ src/components/inbox/Inbox.jsx
@@ -1,38 +1,95 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import './inbox.css';
+import { BsArchive } from "react-icons/bs";
+import { BsTelephoneOutbound } from "react-icons/bs";
+import {BsTelephoneInbound} from "react-icons/bs";
+import "../../css/style.css";
 import axios from "axios";
 
-const Content = () => {
-  const [state, setState] = useState([]);
+const Inbox = () => {
+  const [archive, setArchive] = useState([]);
+  const [activities, setActivities] = useState([]);
+  const [tab, setTab] = useState(0);
 
   useEffect(() => {
     axios
       .get(" https://aircall-job.herokuapp.com/activities")
-      .then((res) => {
-        console.log("response", res.data);
-        setState(res.data);
+      .then(res => {
+        console.log(res.data);
+        setActivities(res.data);
       })
       .catch((error) => console.log(`Error: ${error}`));
   }, []);
 
+  // toggle Tabs
+  const toggleTab = (e) => {
+    e.preventDefault();
+    setTab(e.target.id);
+  };
+  // to archive all calls
+  
+  const archiveAll = (e) => {
+    e.preventDefault();
+    // console.log(e.target.value)
+    activities.forEach((activity) => {
+      const headers = {
+        method: "POST",
+        url: `https://aircall-job.herokuapp.com/activities/${activity.id}`,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ is_archived: false }),
+      };
+
+      fetch(
+        `https://aircall-job.herokuapp.com/activities/${activity.id}`,
+        headers
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res.data);
+          history.go(0);
+        });
+    });
+  }
+
+
   return (
-    <section>
-      {state.length === 0 ? (
-        <div>No response </div>
-      ) : (
-        state.map((call) => {
+    <section className="background">
+      <div className="tabs">
+        <button id={0} className={tab == 0 ? "active" : ""} onClick={toggleTab}>
+          Inbox
+        </button>
+        <button id={1} className={tab == 1 ? "active" : ""} onClick={toggleTab}>
+          Archive
+        </button>
+      </div>
+
+      <button className="archiveAll" onClick={archiveAll}>
+        <BsArchive className="archiveIcon" /> &nbsp;&nbsp;Archive all calls
+      </button>
+
+      {activities.length > 0 ? (
+        activities.map((call) => {
           return (
-            <div key={call.id}>
+            <div className="block btn btn-outline" key={call.id}>
+            
+             
+                {call.direction == "inbound" ? (
+                  <BsTelephoneInbound className="inbound" />
+                ) : (
+                  <BsTelephoneOutbound className="outbound"/>
+                )}
+              
               <div>{call.from} </div>
-              <div> {call.direction} </div>
               <div> {call.created_at} </div>
+            
             </div>
           );
         })
+      ) : (
+        <div>All calls are archived</div>
       )}
     </section>
   );
 };
 
-export default Content;
+export default Inbox;
